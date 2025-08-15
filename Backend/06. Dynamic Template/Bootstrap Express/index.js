26ec4f8106d72dc2bb6a8ca83815ae17b37b810d
amansinh-30 <amansinhparmar30@gmail.com>
2025-08-15 17:03:31 +0530

    Create ejs template using bootstrap and improt files

--- Backend/06. Dynamic Template/Bootstrap Express/index.js
+++ Backend/06. Dynamic Template/Bootstrap Express/index.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 const igData = require("./data.json");
 
+// import css and js file from public folder 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -13,6 +16,18 @@ app
   .get("/random", (req, res) => {
     res.render("random");
   });
+
+app.get("/cars", (req, res) => {
+  const cars = [
+    "BMW",
+    "Lamborghini",
+    "Aston Martin",
+    "McLaren",
+    "Porsche",
+    "Ferrari",
+  ];
+  res.render("car", { cars });
+});
 
 app.get("/:value", (req, res) => {
   const { value } = req.params;
