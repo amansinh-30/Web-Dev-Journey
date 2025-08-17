cabdd335c88eae13b77263dbb01c8e178eeadf4a
amansinh-30 <amansinhparmar30@gmail.com>
2025-08-17 23:10:16 +0530

    learn restful routes

---
+++ Backend/07. Restful Routes/Restful/index.js
@@ -0,0 +1,8 @@
+/*
+GET /comments - list all comments
+POST /comments - Create a new comment
+GET /comments/:id - Get one comment (using ID)
+PATCH /comments/:id - Update one commnet
+DELETE / comments/:id - Distroy one comment
+*/
+
