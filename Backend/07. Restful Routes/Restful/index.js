80933ed7da3f34bc092e5a2a0e4e2e5924cb075a
amansinh-30 <amansinhparmar30@gmail.com>
2025-08-20 22:11:52 +0530

    complete restful routes section

--- Backend/07. Restful Routes/Restful/index.js
+++ Backend/07. Restful Routes/Restful/index.js
@@ -10,51 +10,69 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+// get html files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// get ejs files
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "comments"));
 
+// create an Array
 const comments = [
   {
     username: "Jack",
-    comments: "lol that is so funny",
+    comment: "lol that is so funny",
   },
   {
     username: "Apex",
-    comments: "I just need a break and travel somewhare else",
+    comment: "I just need a break and travel somewhare else",
   },
   {
     username: "Amanada",
-    comments: "I am going to beach this weekend",
+    comment: "I am going to beach this weekend",
   },
   {
     username: "Julia",
-    comments: "My weekend would be hactic",
+    comment: "My weekend would be hactic",
   },
 ];
 
+// for home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "template.html"));
 });
 
-app.get("/comments", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-  // res.render("index", { comments });
-});
+// submit request for 'GET REQUEST'
 app.get("/submit", (req, res) => {
   console.log("GET data:", req.query);
   res.send("This is a GET REQUEST");
 });
+// submit request for 'POST REQUEST'
 app.post("/submit", (req, res) => {
   console.log("POST data:", req.body);
   res.send("This is a POST REQUEST");
 });
 
-app.get("/comment", (req, res) => {
+// Get comment input from Array
+app.get("/comments", (req, res) => {
   res.render("index", { comments });
 });
 
+// Get new comment data from new ejs file
+app.get("/comments/new", (req, res) => {
+  res.render("new");
+});
+
+// Get new comment data with 'POST REQUEST'
+app.post("/comments", (req, res) => {
+  // console.log(req.body);
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.send("YES, IT SUBMITTED!");
+});
+
+// if error found "404"
 app.use((req, res) => {
   res.status(404).send("404 - Page Not Found");
 });
@@ -62,13 +80,3 @@ app.use((req, res) => {
 app.listen(8080, () => {
   console.log("Server Login:8080");
 });
-
-// const commentBtn = document.getElementById("comment");
-
-// const commentList = commentBtn.addEventListener("click", () => {
-//   app.get("/comment", (req, res) => {
-//     res.render("index", { comments });
-//   });
-// });
-
-// commentList();
