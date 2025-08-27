6d275103543daa1872696a5c0b0b9c1b33e6f045
amansinh-30 <amansinhparmar30@gmail.com>
2025-08-27 16:18:45 +0530

    start learning Database

---
+++ Database/index.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/carsCollection");
+// .then(()=>{
+//     console.log('CONNECTION OPEN!');
+// })
+// .catch(err =>{
+//     console.log('OH NO ERROR!!!!');
+//     console.log(err);
+// })
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/carsCollection/exotic_car");
+  console.log("CONNECTED!!");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const carSchema = new mongoose.Schema({
+  name: String,
+  model: String,
+  year: Number,
+  horsePower: String,
+});
+
+const Car = mongoose.model("exotic_car", carSchema);
+// const Cadillac = new Car({
+//   name: "Cadillac",
+//   model: "Escalade ESV",
+//   year: 2023,
+//   horsePower: " 682 hp",
+// });
+
+// Car.insertMany([
+//   { name: "Lamborghini", model: "Urus", year: 2022, horsePower: "650 hp" },
+//   { name: "Aston Martin", model: "DBX", year: 2020, horsePower: "542 hp" },
+//   { name: "Porsche", model: "Cayenne", year: 2021, horsePower: "541 hp" },
+//   { name: "Rolls-Royce ", model: "Cullinan", year: 2023, horsePower: "563 hp" },
+// ]);
+
+
